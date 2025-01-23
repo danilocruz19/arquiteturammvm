84b1cbca2fa6f564915295e59c962e5ea9d8cf34
danilocruz19 <danilosouzaflamengo19@gmail.com>
2025-01-23 13:29:19 -0300

    First commit

---
+++ android/app/src/main/kotlin/com/example/mvvc_arquitetura/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mvvc_arquitetura
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
